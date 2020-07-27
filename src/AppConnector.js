1f1dcf2b99911497381f46fc94d2f8296a441e6b
Sharon Mathews <shmathew@microsoft.com>
2020-07-27 19:32:37 +0530

    Initial working

---
+++ src/AppConnector.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {MAFIA_STATES, useGlobalState} from './App';
+import AppRouter from "./AppRouter";
+import * as signalR from '@microsoft/signalr';
+
+function AppConnector() {
+  const [state, dispatch] = useGlobalState();
+  let connection = state.connection;
+  if (!connection) {
+    try {
+      connection = new signalR.HubConnectionBuilder()
+        .withUrl("https://mafiaapp20200725162443.azurewebsites.net/chathub")
+        .build();
+        connection.start({ withCredentials: false, sameSite: "Lax" }).catch((error) => { console.log(error)} )
+        dispatch({mafiaScreen: MAFIA_STATES.LOGIN, connection: connection})
+    } catch(error) {
+      console.log(error);
+    }
+  }
+  return (
+    <AppRouter />
+  );
+}
+export default AppConnector;
