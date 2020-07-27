66bd1dd09ecc79811625e6f18544c74ea33c11a3
Vijitha Gunta <vigunta@microsoft.com>
2020-07-27 22:11:29 +0530

    styling player row (width alignment,removing hard-coding pending)

--- src/GameLayout/GameLayout.js
+++ src/GameLayout/GameLayout.js
@@ -6,7 +6,7 @@ import { containerStyles } from "./GameLayoutStyles";
 function GameLayout() {
   return (
       <div style={containerStyles()}>
-        <PlayersList />
+        <PlayersList players = {[{name:"suzie",role:"mafia",alive:"dead", vote:"5"}, {name:"suzie2",role:"villager",alive:"alive", vote:"89"}]} roundState = "Day" isPrimaryMafia = "true" currentPlayerName = "suzie" />
         <div>Mafia is killing</div>
         <ChatPane />
       </div>
--- src/PlayersList/PlayersList.js
+++ src/PlayersList/PlayersList.js
@@ -1,10 +1,48 @@
-import React from 'react';
+import React, { Component } from "react";
 import { containerStyles } from "./PlayersListStyles";
-function PlayersList() {
-  return (
-    <div style={containerStyles()}>
-    </div>
-  );
+import { ChoiceGroup} from 'office-ui-fabric-react/lib/ChoiceGroup';
+
+class PlayersList extends Component {
+  render() {
+    const options1 = [
+       { key: 'B', text: '',
+      onRenderField: (props, render) => {
+          return (
+            <div style = {{display:"flex",alignItems:"center",margin:"10px", justifyContent:"space-between", minWidth:"330px"}}>
+              <img style= {{height:"50px", borderRadius:"50%"}} src="https://api.adorable.io/avatars/viji.png"></img>
+              <div style = {{fontFamily:"arial", fontSize:"20px"}}>Suzie</div>
+              <div style = {{fontFamily:"arial", fontSize:"20px", color:"#BF2626"}}>+5</div>
+              {render(props)}    
+            </div>
+          );
+        } 
+      ,styles: { root: { backgroundColor:"rgba(247,105,105,0.56)"}}},
+    { key: 'C', text: '',
+      onRenderField: (props, render) => {
+          return (
+            <div style = {{display:"flex",alignItems:"center",margin:"10px"}}>
+              
+              <img style= {{height:"30px", borderRadius:"50%"}} src="https://api.adorable.io/avatars/viji.png"></img>
+    {render(props)}
+            </div>
+          );
+        } 
+      ,styles: { root: { backgroundColor:"red"}}},
+    { key: 'D', text: '',
+      onRenderField: (props, render) => {
+          return (
+            <div style = {{display:"flex",flexDirection:"row-reverse",alignItems:"center",margin:"10px"}}>
+              {render(props)}
+              <img style= {{height:"30px", borderRadius:"50%"}} src="https://api.adorable.io/avatars/viji.png"></img>
+            </div>
+          );
+        } 
+      ,styles: { root: { backgroundColor:"red"}}},
+    ];
+    return <div style={containerStyles()}>
+      <ChoiceGroup options={options1}/>
+    </div>;
+  }
 }
 
 export default PlayersList;
