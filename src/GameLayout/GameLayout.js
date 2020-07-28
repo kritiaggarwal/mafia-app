7546c03cff30cd9e0c402efb5890cbaa0f943dbe
Vijitha Gunta <vigunta@microsoft.com>
2020-07-28 14:53:53 +0530

    props sync

--- src/GameLayout/GameLayout.js
+++ src/GameLayout/GameLayout.js
@@ -7,14 +7,14 @@ function GameLayout() {
   return (
       <div style={containerStyles()}>
         <PlayersList players = {
-          [{name:"Suzie",role:"mafia",alive:"alive", vote:"1"}, 
-          {name:"Viji",role:"mafia",alive:"dead", vote:"2"}, 
-          {name:"Sharon",role:"villager",alive:"dead", vote:"1"},
-          {name:"Kriti",role:"villager",alive:"alive", vote:"0"},
-          {name:"Joyeeta",role:"villager",alive:"alive", vote:"2"}, 
-          {name:"Neeli",role:"mafia",alive:"dead", vote:"0"}]
+          [{name:"Suzie",role:"mafia",alive:"alive", vote:"1", playerId:"1"}, 
+          {name:"Viji",role:"mafia",alive:"dead", vote:"2", playerId:"2"}, 
+          {name:"Sharon",role:"villager",alive:"dead", vote:"1", playerId:"3"},
+          {name:"Kriti",role:"villager",alive:"alive", vote:"0", playerId:"4"},
+          {name:"Joyeeta",role:"villager",alive:"alive", vote:"2", playerId:"5"}, 
+          {name:"Neeli",role:"mafia",alive:"dead", vote:"0", playerId:"6"}]
           } 
-          roundState = "day" isPrimaryMafia = "true" currentPlayerName = "Suzie" />
+          roundState = "day" isPrimaryMafia = "true" currentPlayerName = "Suzie" isCurrentPlayerAlive = "true" />
         <div>Mafia is killing</div>
         <ChatPane />
       </div>
--- src/PlayersList/PlayersList.js
+++ src/PlayersList/PlayersList.js
@@ -3,30 +3,47 @@ import { containerStyles } from "./PlayersListStyles";
 import { ChoiceGroup} from 'office-ui-fabric-react/lib/ChoiceGroup';
 
 class PlayersList extends Component {
+
+  getPlayerRows(showButtonForGeneralVoting, showButtonForMafiaVoting) {
+    let playerRows = [];
+    for(player in this.props.players){
+      let showButton = false;
+      if ((showButtonForGeneralVoting && player.alive == 'alive') || 
+      showButtonForMafiaVoting && player.alive == 'alive' && player.role == "villager") {
+        showButton = true;
+      }
+      playerRow = { key: player.playerId , text: '',
+        onRenderField: (props, render) => {
+            return (
+              <div style = {{display:"flex",alignItems:"center",margin:"10px", justifyContent:"space-between", minWidth:"330px"}}>
+                <img style= {{height:"50px", borderRadius:"50%"}} src={"https://api.adorable.io/avatars/" + player.name + ".png"}></img>
+                <div style = {{fontFamily:"arial", fontSize:"20px"}}>{player.name}</div>
+                <div style = {{fontFamily:"arial", fontSize:"20px", color:"#BF2626", 
+                visibility: player.vote ? "visible": "hidden"}}>{player.vote}</div>
+                <div style = {{visibility: showButton?"visible":"hidden"}}>{render(props)}</div>
+              </div>
+            );
+          } 
+        ,styles: { root: { backgroundColor:"rgba(247,105,105,0.56)"}}}; //todo - background, scrolling, callbacks, row css
+      playerRows.push(playerRow);
+    }
+    return playerRows;
+  }
+
   render() {
+    let showButtonForGeneralVoting = false;
+    let showButtonForMafiaVoting = false;
+
     if (this.props.roundState == "day" && this.props.isCurrentPlayerAlive) {
       showButtonForGeneralVoting = true;
     }  else if (this.props.roundState == "night" && this.props.isPrimaryMafia) {
       showButtonForMafiaVoting = true;
     }
     
-
+    const options = this.getPlayerRows(showButtonForGeneralVoting, showButtonForMafiaVoting);
     
-    const options1 = [
-       { key: 'A', text: '',
-      onRenderField: (props, render) => {
-          return (
-            <div style = {{display:"flex",alignItems:"center",margin:"10px", justifyContent:"space-between", minWidth:"330px"}}>
-              <img style= {{height:"50px", borderRadius:"50%"}} src="https://api.adorable.io/avatars/viji.png"></img>
-              <div style = {{fontFamily:"arial", fontSize:"20px"}}>{this.props.players[1].name}</div>
-              <div style = {{fontFamily:"arial", fontSize:"20px", color:"#BF2626", visibility: "visible"}}>+5</div>
-              <div style = {{visibility: "visible"}}>{render(props)}</div>
-            </div>
-          );
-        } 
-      ,styles: { root: { backgroundColor:"rgba(247,105,105,0.56)"}}}];
     return <div style={containerStyles()}>
-      <ChoiceGroup options={options1}/>
+      <ChoiceGroup options={options}/>
     </div>;
   }
 }
